594786563212619ae2894a83fac59bfc67a582d0
Chandrasekaran21 <schandru2115@gmail.com>
2026-09-04 22:03:06 +0530

    Theme Updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
                 <Link to='/' className="btn btn-ghost text-xl">ShopEase</Link>
           </div>
 
-        <div className="flex-none">
+        <div className="flex-none flex items-center gap-2">
           <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-96 " />
           <ul className="menu menu-horizontal px-1">
             <li><Link to="/cart" >Cart 
@@ -38,18 +38,39 @@ const Header = () => {
             </li>
             <li>
               <details className="dropdown dropdown-end" >
-                <summary >{theme}</summary>
-                <ul className="bg-base-100 rounded-box w-52 z-[1] p-2 dropdown-content menu shadow">
-                  {themes.map((theme)=>(
-
-                    <li key={theme} onClick={() => changeTheme(theme)}>
-                      {theme}
-                    </li>
-                  ))}
-                </ul>
+                <summary >
+                  Chandru
+                </summary>
               </details>
             </li>
           </ul>
+          <label className="toggle text-base-content" style={{ '--tglbg': '#2563eb' }}
+              onClick={(e) => e.stopPropagation()}>
+                  <input
+                    type="checkbox"
+                    className="theme-controller"
+                    checked={theme === 'dark'}
+                    onChange={(e) => changeTheme(e.target.checked ? 'dark' : 'light')}
+                  />
+                  <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor">
+                      <circle cx="12" cy="12" r="4"></circle>
+                      <path d="M12 2v2"></path>
+                      <path d="M12 20v2"></path>
+                      <path d="m4.93 4.93 1.41 1.41"></path>
+                      <path d="m17.66 17.66 1.41 1.41"></path>
+                      <path d="M2 12h2"></path>
+                      <path d="M20 12h2"></path>
+                      <path d="m6.34 17.66-1.41 1.41"></path>
+                      <path d="m19.07 4.93-1.41 1.41"></path>
+                    </g>
+                  </svg>
+                  <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor">
+                      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                    </g>
+                  </svg>
+                </label>
         </div>
       </div>
   )
