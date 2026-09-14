f5459e8adcb3891ed6390cd739c20653be83a415
Chandrasekaran21 <schandru2115@gmail.com>
2026-09-14 16:33:12 +0530

    base_url name updated for deploy

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL ="http://localhost:5000/";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/";
 export const PRODUCT_URL = "/api/product";
 export const USER_URL = "/api/users";
 export const ORDER_URL = "/api/orders";
